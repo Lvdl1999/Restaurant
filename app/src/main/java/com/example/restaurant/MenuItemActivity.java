1e4354ec2bfac2aa4d77c6b03d6e5b7213358cfe
levyvanderlinde <l-vdl@outlook.com>
2019-05-25 20:00:43 +0200

    restaurant again

--- app/src/main/java/com/example/restaurant/MenuItemActivity.java
+++ app/src/main/java/com/example/restaurant/MenuItemActivity.java
@@ -6,6 +6,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.restaurant.MenuItem;
+import com.example.restaurant.R;
+
 import org.w3c.dom.Text;
 
 public class MenuItemActivity extends AppCompatActivity {
